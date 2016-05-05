91932a9213ed7cd06a0dc56f6862cb3cead85a74
dr0l3 <runedrolepedersen@gmail.com>
2016-05-05 15:48:59 +0200

    fixed a timestamp issue in daily updates

--- updateDatabaseWithDailyUpdates.py
+++ updateDatabaseWithDailyUpdates.py
@@ -22,8 +22,8 @@ for game in list_of_game_names:
     game_set_name = game + "-average_every_hour"
 
     # oldest timestamp is 30 days old
-    current_time_plus_a_day = current_date + (60 * 60 * 24)
-    current_time_plus_30_days = current_date + (60 * 60 * 24 * 30)
+    current_time_plus_a_day = current_date - (60 * 60 * 24)
+    current_time_plus_30_days = current_date - (60 * 60 * 24 * 30)
     db.zremrangebyscore(game_set_name, "-inf", current_time_plus_30_days)
     list_of_events = db.zrangebyscore(game_set_name, current_date, current_time_plus_a_day)
 
